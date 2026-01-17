6033dc90997e89fa5f5635111e6a3d56fc505951
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-01-17 13:05:21 +0500

    create notesList object

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,30 @@
-function saveToStorage() {
+let nextId =1;
 
-}
+function createNote(textContent) {
+  const note = {
+    title: `Note ${nextId}`,
+    id: nextId,
+    body: textContent
+  };
+  nextId++;
+  return note;
+};
+
+const notesList = []
+
+
+const saveButton = document.querySelector('.js-save-button');
+
+saveButton.addEventListener('click', () => {
+  const noteContent = document.querySelector('.js-note-pad').value;
+  
+  const newNote = createNote(noteContent);
+
+  notesList.push(newNote);
+  console.log(notesList)
+
+});
+
+
+
+

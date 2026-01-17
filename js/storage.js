a1b3504603306cdeb8cf10bb52f36215b099c841
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-01-17 14:09:28 +0500

    added the title inside object

--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,9 @@
-let nextId =1;
+let notesList = JSON.parse(localStorage.getItem('notesList')) ||[];
+let nextId = notesList.length > 0 ? notesList[notesList.length -1].id + 1 : 1;
 
-function createNote(textContent) {
+function createNote(textContent, title) {
   const note = {
-    title: `Note ${nextId}`,
+    title: title || (textContent ? textContent.substring(0, 8) + '...' : `Note${nextId}`),
     id: nextId,
     body: textContent
   };
@@ -10,21 +11,29 @@ function createNote(textContent) {
   return note;
 };
 
-const notesList = []
-
 
 const saveButton = document.querySelector('.js-save-button');
 
 saveButton.addEventListener('click', () => {
   const noteContent = document.querySelector('.js-note-pad').value;
+  const notesTitle = document.querySelector('.js-notes-title').value;
   
-  const newNote = createNote(noteContent);
+  const newNote = createNote(noteContent, notesTitle);
 
   notesList.push(newNote);
   console.log(notesList)
+  saveToStorage();
+  resetPage();
 
 });
 
+function resetPage () {
+  window.location.reload()
+}
+
+function saveToStorage() {
+  localStorage.setItem('notesList', JSON.stringify(notesList));
+}
 
 
-
+console.log(JSON.parse(localStorage.getItem('notesList')))

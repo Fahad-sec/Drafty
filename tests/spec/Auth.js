9c7bf10183d24074072c14de083f79cb4d35646b
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-01-30 16:15:34 +0500

    v2.9.20 fix: resolve environment variable errors and update test paths in Specrunner

--- tests/spec/Auth.js
+++ tests/spec/Auth.js
@@ -1,8 +1,8 @@
-/*import {} from '../../js/storage.js'
+import {setupAuthListeners} from '../../js/storage.js'
 
 describe ('test suite: authentication',() => {
     
   it ('login test', () => {
 
   })
-})*/
+})
